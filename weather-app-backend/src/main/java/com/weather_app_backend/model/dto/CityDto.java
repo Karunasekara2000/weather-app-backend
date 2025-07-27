d1802889e5a573d915435a1710ce2283c2918082
lahiruSandaruwan20 <lahiru.20210390@iit.ac.lk>
2025-07-27 15:01:11 +0530

    created city dto.

---
+++ weather-app-backend/src/main/java/com/weather_app_backend/model/dto/CityDto.java
@@ -0,0 +1,19 @@
+package com.weather_app_backend.model.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CityDto {
+    private String name;
+}
